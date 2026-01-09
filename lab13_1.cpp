8cb54f9c7e309bfb8ccb2dccc6eecdc8f0dbde69
pojnaja <wasarn519@gmail.com>
2026-01-09 19:18:01 +0700

    Initial commit

---
+++ lab13_1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+void stat(const double[],int,double[]);
+
+int main(){
+    double A[] = {1.2,3.5,6.9,7.8,12.5,0.5};
+    int N = sizeof(A)/sizeof(A[0]);
+    double B[6];
+    stat(A,N,B);
+    cout << fixed << setprecision(2);
+    cout << "Arithmetic Mean = " << B[0] << endl;
+    cout << "Standard Deviation = " << B[1] << endl;
+    cout << "Geometric Mean = " << B[2] << endl;
+    cout << "Harmonic Mean = " << B[3] << endl;
+    cout << "Max = " << B[4] << endl;
+    cout << "Min = " << B[5];
+    return 0;
+}
